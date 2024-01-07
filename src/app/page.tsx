8dc6896cc9127332cf247312d6b1289a89eb53ec
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2024-01-07 19:24:37 +0000

    client side fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 "use client"
+import { useState, useEffect } from 'react'
 import IpDisplay from './(components)/ip'
 import { IP } from './(types)/ip'
 
-export default async function Home() {
+
+export default function Home() {
   
-   const data = await fetch('https://api.my-ip.io/v2/ip.json',{cache:'no-store'})
-  const ip = await data.json() as IP
+  
+  const [data, setData] = useState<IP>()
+  const [isLoading, setLoading] = useState(true)
  
+  useEffect(() => {
+    fetch('https://api.my-ip.io/v2/ip.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+ 
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-items-center justify-center p-24">
@@ -14,7 +28,7 @@ export default async function Home() {
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]">
         </div>
       </div>
-      <IpDisplay ipInfo={ip}/>
+      <IpDisplay ipInfo={data} />
     </main>
   )
 }
